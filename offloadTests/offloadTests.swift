ddedb71cc66fd786c9b7cd9183408328568a68fd
William Conklin <will.conklin@gmail.com>
2025-12-30 22:52:18 -0500

    Initial Commit

---
+++ offloadTests/offloadTests.swift
@@ -0,0 +1,17 @@
+//
+//  offloadTests.swift
+//  offloadTests
+//
+//  Created by William Conklin on 12/30/25.
+//
+
+import Testing
+@testable import offload
+
+struct offloadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
